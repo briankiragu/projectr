798da35697c9130104ec28c19681311ce3311bde
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-02-16 13:32:34 +0300

    feat: Fix mobile responsiveness jank

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { For, createSignal } from "solid-js";
-import "./App.css";
-import type { ITrack } from "./interfaces/track";
+import { For, Show, createSignal } from 'solid-js';
+import './App.css';
+import type { ITrack } from './interfaces/track';
 
 // Sample data.
-import data from "./data/sample";
+import data from './data/sample';
 
 function App() {
   const [results] = createSignal<ITrack[]>(data);
@@ -25,168 +25,187 @@ function App() {
 
   // JSX component.
   return (
-    <>
-      <div class="grid gap-4 p-3 lg:grid-cols-4">
-        <div class="grid gap-4">
-          {/* Search Pane */}
-          <form class="grid h-72 grid-cols-1 rounded-lg bg-gray-300 px-4 pb-4 pt-3">
-            <label for="search">
-              <span class="text-sm italic text-gray-800">
-                Search for a song by title or lyrics...
-              </span>
-              <input
-                id="search"
-                type="search"
-                name="search"
-                class="mt-1 w-full rounded-lg px-4 py-3 text-sm text-gray-600 focus:outline-none"
-                placeholder="Search for a song by title or lyrics..."
-              />
+    // Main container
+    <div class="grid gap-4 overflow-hidden p-3 lg:h-screen lg:grid-cols-4">
+      <aside class="h-[88%] flex-col justify-between rounded-lg bg-gray-100">
+        {/* Search Pane */}
+        <form class="mb-4 h-80 rounded-lg bg-gray-300 px-4 pb-4 pt-3">
+          <label for="search">
+            <span class="text-sm italic text-gray-800">
+              Search for a song by title or lyrics...
+            </span>
+            <input
+              id="search"
+              type="search"
+              name="search"
+              class="mt-1 w-full rounded-lg px-4 py-3 text-sm text-gray-600 focus:outline-none"
+              placeholder="Search for a song by title or lyrics..."
+              autofocus
+            />
 
-              {/* Search results */}
-              <div class="mt-5">
-                <For each={results()}>
-                  {(track) => (
-                    <div class="mb-1.5 flex justify-between gap-4 rounded-lg bg-gray-100 px-3 py-2 text-sm text-gray-600">
-                      <span class="material-symbols-outlined cursor-move p-1">
-                        drag_pan
-                      </span>
-                      <h4 class="col-span-2 w-full py-1.5 font-medium italic text-gray-500">
-                        {track.title}
-                      </h4>
-                      <button
-                        type="button"
-                        class="material-symbols-outlined rounded-full p-1 hover:bg-gray-300"
-                        onClick={() => enqueue(track)}
-                      >
-                        playlist_add
-                      </button>
-                    </div>
-                  )}
-                </For>
-              </div>
-            </label>
-          </form>
+            {/* Search results */}
+            <div class="mt-3">
+              <For each={results()}>
+                {(track) => (
+                  <div class="mb-1.5 flex justify-between gap-4 rounded-lg bg-gray-100 px-3 py-2 text-sm text-gray-600">
+                    <span class="material-symbols-outlined cursor-move p-1">
+                      drag_pan
+                    </span>
+                    <h4 class="col-span-2 w-full py-1.5 font-medium italic text-gray-500">
+                      {track.title}
+                    </h4>
+                    <button
+                      type="button"
+                      class="material-symbols-outlined rounded-full p-1 hover:bg-gray-300"
+                      onClick={() => enqueue(track)}
+                    >
+                      playlist_add
+                    </button>
+                  </div>
+                )}
+              </For>
+            </div>
+          </label>
+        </form>
 
-          {/* Play queue */}
-          <div class="rounded-lg bg-gray-200 px-4 pb-4 pt-3">
-            {/* Now playing */}
-            <div class="h-24">
-              <h3 class="mb-2 text-sm text-gray-400">Now playing</h3>
+        {/* Play queue */}
+        <div class="rounded-lg bg-gray-200 px-4 pb-4 pt-3 lg:h-3/6">
+          {/* Now playing */}
+          <div class="h-24">
+            <h3 class="mb-2 text-sm text-gray-400">Now playing</h3>
+            <Show when={nowPlaying()}>
               <div class="grid grid-cols-4 rounded-lg bg-gray-100 px-6 py-3 text-sm">
                 <h4 class="col-span-2 font-semibold text-gray-800">
                   {nowPlaying()?.title}
                 </h4>
               </div>
-            </div>
+            </Show>
+          </div>
 
-            {/* Up next */}
-            <div>
-              <h3 class="mb-2 text-sm text-gray-400">Up next</h3>
-              <div>
-                <For each={queue().slice(1)}>
-                  {(track) => (
-                    <div class="mb-2 flex justify-between gap-4 rounded-lg bg-gray-100 px-4 py-2 align-middle text-sm text-gray-600">
-                      <span class="material-symbols-outlined cursor-move p-1">
-                        drag_pan
-                      </span>
-                      <h4 class="col-span-2 w-full py-1 font-semibold">
-                        {track.title}
-                      </h4>
-                      <button
-                        class="material-symbols-outlined rounded-full p-1 hover:bg-gray-300"
-                        onClick={() => dequeue(track.id)}
-                      >
-                        playlist_remove
-                      </button>
-                    </div>
+          {/* Up next */}
+          <div class="">
+            <h3 class="text-sm text-gray-400">Up next</h3>
+            <div class="h-2/5 overflow-scroll">
+              <For each={queue().slice(1)}>
+                {(track) => (
+                  <div class="mt-2 flex justify-between gap-4 rounded-lg bg-gray-100 px-4 py-2 align-middle text-sm text-gray-600">
+                    <span class="material-symbols-outlined cursor-move p-1">
+                      drag_pan
+                    </span>
+                    <h4 class="col-span-2 w-full py-1 font-semibold">
+                      {track.title}
+                    </h4>
+                    <button
+                      class="material-symbols-outlined rounded-full p-1 hover:bg-gray-300"
+                      onClick={() => dequeue(track.id)}
+                    >
+                      playlist_remove
+                    </button>
+                  </div>
+                )}
+              </For>
+            </div>
+          </div>
+        </div>
+      </aside>
+
+      {/* View Pane */}
+      <main class="mb-32 rounded-lg bg-gray-100 px-6 py-4 lg:col-span-3 lg:mb-20 lg:px-12 lg:py-10">
+        {/* Title */}
+        <h2 class="mb-3 text-wrap text-4xl font-black text-gray-800 lg:text-5xl">
+          {nowPlaying()?.title}
+        </h2>
+
+        {/* Lyrics */}
+        <div class="text-md font-medium text-gray-600">
+          <For
+            each={[
+              nowPlaying()?.lyrics.intro,
+              ...(nowPlaying()?.lyrics.verses || []),
+            ]}
+          >
+            {(verse) => (
+              <ul class="mb-3">
+                <For each={verse}>
+                  {(line) => (
+                    <li>
+                      <span class="italic">{line}</span>
+                    </li>
                   )}
                 </For>
-              </div>
-            </div>
-          </div>
+              </ul>
+            )}
+          </For>
         </div>
 
-        {/* View Pane */}
-        <div class="mb-20 rounded-lg bg-gray-100 px-6 py-4 md:col-span-3">
-          {/* Title */}
-          <h2 class="mb-3 text-5xl font-black text-gray-800">
-            {nowPlaying()?.title}
-          </h2>
-
-          {/* Lyrics */}
-          <div class="text-md font-medium text-gray-600">
-            <For each={nowPlaying()?.lyrics.verses}>
-              {(verse) => (
-                <ul class="mb-3">
-                  <For each={verse}>
-                    {(line) => (
-                      <li>
-                        <span class="italic">{line}</span>
-                      </li>
-                    )}
-                  </For>
-                </ul>
-              )}
-            </For>
-          </div>
-
-          {/* Controls */}
-          <section class="fixed bottom-0 left-0 w-full bg-white p-3">
-            <div class="flex h-16 justify-between gap-4 rounded-lg bg-gray-200 p-4 text-gray-700 lg:justify-center">
-              {/* <button class="h-8 w-8 rounded-full transition-all hover:bg-gray-400">
+        {/* Controls */}
+        <footer class="fixed bottom-0 left-0 w-full bg-white p-3">
+          <div class="flex min-h-16 flex-wrap justify-between gap-4 rounded-lg bg-gray-200 p-4 text-gray-700 lg:justify-center">
+            {/* <button class="h-8 w-8 rounded-full transition-all hover:bg-gray-400">
               <span class="material-symbols-outlined">skip_previous</span>
             </button> */}
-              <button
-                class="p-1 h-8 w-8 rounded-full transition-colors hover:bg-gray-400"
-                classList={{
-                  "hover:bg-gray-200 disabled:text-gray-300": !nowPlaying(),
-                }}
-                disabled={!nowPlaying()}
-              >
-                <span class="material-symbols-outlined">arrow_back</span>
-              </button>
-              <button
-                class="p-1 h-8 w-8 rounded-full transition-colors hover:bg-gray-400"
-                classList={{
-                  "hover:bg-gray-200 disabled:text-gray-300": !nowPlaying(),
-                }}
-                disabled={!nowPlaying()}
-              >
-                <span class="material-symbols-outlined">arrow_forward</span>
-              </button>
-              <button
-                class="p-1 h-8 w-8 rounded-full transition-colors hover:bg-gray-400"
-                classList={{
-                  "hover:bg-gray-200 disabled:text-gray-300": !nowPlaying(),
-                }}
-                disabled={!nowPlaying()}
-                onClick={() => dequeue(nowPlaying()?.id)}
-              >
-                <span class="material-symbols-outlined ">skip_next</span>
-              </button>
-              <button
-                class="rounded-md px-2.5 transition-colors hover:bg-gray-400  hover:text-gray-50"
-                classList={{
-                  "hover:bg-gray-200 disabled:text-gray-300": !nowPlaying(),
-                }}
-                disabled={!nowPlaying()?.lyrics.bridge}
-              >
-                Bridge
-              </button>
-              <button
-                class="rounded-md px-2.5 transition-colors hover:bg-gray-400  hover:text-gray-50"
-                classList={{
-                  "hover:bg-gray-200 disabled:text-gray-300": !nowPlaying(),
-                }}
-                disabled={!nowPlaying()?.lyrics.chorus}
-              >
-                Chorus
-              </button>
-            </div>
-          </section>
-        </div>
-      </div>
-    </>
+            <button
+              class="h-8 w-8 rounded-full p-1 transition-colors hover:bg-gray-400"
+              classList={{
+                'hover:bg-gray-200 disabled:text-gray-300': !nowPlaying(),
+              }}
+              disabled={!nowPlaying()}
+            >
+              <span class="material-symbols-outlined">arrow_back</span>
+            </button>
+            <button
+              class="h-8 w-8 rounded-full p-1 transition-colors hover:bg-gray-400"
+              classList={{
+                'hover:bg-gray-200 disabled:text-gray-300': !nowPlaying(),
+              }}
+              disabled={!nowPlaying()}
+            >
+              <span class="material-symbols-outlined">arrow_forward</span>
+            </button>
+            <button
+              class="h-8 w-8 rounded-full p-1 transition-colors hover:bg-gray-400"
+              classList={{
+                'hover:bg-gray-200 disabled:text-gray-300': !nowPlaying(),
+              }}
+              disabled={!nowPlaying()}
+              onClick={() => dequeue(nowPlaying()?.id)}
+            >
+              <span class="material-symbols-outlined ">skip_next</span>
+            </button>
+            <button
+              class="rounded-md px-2.5 transition-colors hover:bg-gray-400  hover:text-gray-50"
+              classList={{
+                'hover:bg-gray-200 disabled:text-gray-300':
+                  !nowPlaying()?.lyrics.preChorus,
+              }}
+              disabled={!nowPlaying()?.lyrics.preChorus}
+            >
+              Pre-Chorus
+            </button>
+            <button
+              class="rounded-md px-2.5 transition-colors hover:bg-gray-400  hover:text-gray-50"
+              classList={{
+                'hover:bg-gray-200 disabled:text-gray-300':
+                  !nowPlaying()?.lyrics.chorus,
+              }}
+              disabled={!nowPlaying()?.lyrics.chorus}
+            >
+              Chorus
+            </button>
+            <button
+              class="rounded-md px-2.5 transition-colors hover:bg-gray-400  hover:text-gray-50"
+              classList={{
+                'hover:bg-gray-200 disabled:text-gray-300':
+                  !nowPlaying()?.lyrics.bridge,
+              }}
+              disabled={!nowPlaying()?.lyrics.bridge}
+            >
+              Bridge
+            </button>
+          </div>
+        </footer>
+      </main>
+    </div>
   );
 }
 
