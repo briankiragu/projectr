02d24c85f25522a6c1d906ff10c717eaeaea54d1
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-02-16 14:26:47 +0300

    feat: Add shadows to list items

--- src/ui/queue/QueueItem.tsx
+++ src/ui/queue/QueueItem.tsx
@@ -8,7 +8,7 @@ const QueueItem: Component<{
   const [fn, args] = props.handler;
 
   return (
-    <div class="mt-2 flex justify-between gap-4 rounded-lg bg-gray-100 px-4 py-2 align-middle text-sm text-gray-600">
+    <div class="mt-2 flex justify-between gap-4 rounded-lg bg-gray-100 px-4 py-2 align-middle text-sm text-gray-600 shadow transition-shadow hover:shadow-md">
       <span class="material-symbols-outlined cursor-move p-1">drag_pan</span>
       <h4 class="col-span-2 w-full py-1 font-semibold">{props.track.title}</h4>
       <button
