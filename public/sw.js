72dfba627ffa0e51bd5721315cd977e92b5fbdb1
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-02-18 21:29:51 +0300

    feat: Update service worker formatting

--- public/sw.js
+++ public/sw.js
@@ -2,11 +2,11 @@ const cacheName = 'projectr-v1.0.0';
 const appShellFiles = ['/logo.webp'];
 
 self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
+  console.info('[Service Worker] Install');
   e.waitUntil(
     (async () => {
       const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
+      console.info('[Service Worker] Caching all: app shell and content');
       await cache.addAll([...appShellFiles]);
     })()
   );
@@ -31,36 +31,30 @@ self.addEventListener('activate', (e) => {
 //   e.respondWith(
 //     (async () => {
 //       const r = await caches.match(e.request);
-//       console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+//       console.info(`[Service Worker] Fetching resource: ${e.request.url}`);
 //       if (r) {
 //         return r;
 //       }
 //       const response = await fetch(e.request);
 //       const cache = await caches.open(cacheName);
-//       console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+//       console.info(`[Service Worker] Caching new resource: ${e.request.url}`);
 //       cache.put(e.request, response.clone());
 //       return response;
 //     })()
 //   );
 // });
 
-self.addEventListener('fetch', (event) => {
-  console.dir(event.request);
-  event.respondWith(
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
     caches.open(cacheName).then(async (cache) => {
-      const cachedResponse = await cache.match(event.request);
-      const fetchedResponse = fetch(event.request).then((networkResponse) => {
-        cache.put(event.request, networkResponse.clone());
+      console.info(`[Service Worker] Fetching resource: ${e.request.url}`);
+
+      const cachedResponse = await cache.match(e.request);
+      const fetchedResponse = fetch(e.request).then((networkResponse) => {
+        cache.put(e.request, networkResponse.clone());
 
         return networkResponse;
       });
-
-      if (cachedResponse) {
-        console.log('Cached response');
-      }
-      if (fetchedResponse) {
-        console.log('Fetched response');
-      }
 
       return cachedResponse || fetchedResponse;
     })
