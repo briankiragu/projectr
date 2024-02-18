1985e5b73a47bf5953b95a22b096f06acb0a7dfa
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-02-18 21:21:18 +0300

    feat: Add uppercase to main preview

--- src/ui/cards/LyricsPreviewCard.tsx
+++ src/ui/cards/LyricsPreviewCard.tsx
@@ -7,7 +7,9 @@ const LyricsPreviewCard: Component<{
     <div class="h-auto min-h-60 rounded-lg bg-teal-600 px-6 py-4 text-teal-50 shadow-lg shadow-teal-600/20 transition-colors lg:h-72">
       <For each={props.verse}>
         {(line) => (
-          <p class="text-wrap text-xl font-semibold lg:text-2xl">{line}</p>
+          <p class="text-wrap text-xl font-semibold uppercase lg:text-2xl">
+            {line}
+          </p>
         )}
       </For>
     </div>
