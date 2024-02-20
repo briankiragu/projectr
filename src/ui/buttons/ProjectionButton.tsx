ed45fc4465128dd0404d6e1fa196a89aa4b6f48a
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-02-20 22:21:37 +0300

    feat: Change projection button click event element

--- src/ui/buttons/ProjectionButton.tsx
+++ src/ui/buttons/ProjectionButton.tsx
@@ -9,10 +9,11 @@ const ProjectionButton: Component<{ handler?: () => void }> = (props) => {
 
   return (
     <Show when={isSupported}>
-      <div class="flex h-10 w-10 cursor-pointer gap-3 rounded-full bg-teal-300 p-2 text-teal-800 transition-colors hover:bg-teal-700 hover:text-teal-50 lg:h-auto lg:w-auto lg:px-6">
-        <span class="material-symbols-outlined" onClick={props.handler}>
-          screen_share
-        </span>
+      <div
+        class="flex h-10 w-10 cursor-pointer gap-3 rounded-full bg-teal-300 p-2 text-teal-800 transition-colors hover:bg-teal-700 hover:text-teal-50 lg:h-auto lg:w-auto lg:px-6"
+        onClick={props.handler}
+      >
+        <span class="material-symbols-outlined">screen_share</span>
         <span class="hidden font-semibold lg:inline">Project</span>
       </div>
     </Show>
