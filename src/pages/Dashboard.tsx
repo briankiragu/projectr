f1d9e137e607d5cb7ea8d81826bb1eb2308c2aa4
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-02-21 19:20:48 +0300

    feat: Add preloaders

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -189,12 +189,18 @@ const App: Component = () => {
           </Show>
         </div>
 
+        {/* Lyrics Preloader */}
+        <Show when={!peek()}>
+          <div class="h-42 grid grid-cols-1 gap-4 lg:h-80 lg:grid-cols-3 lg:overflow-y-scroll lg:pb-2">
+            <div class="rounded-md bg-gray-200/40"></div>
+            <div class="rounded-md bg-gray-200/40"></div>
+            <div class="rounded-md bg-gray-200/40"></div>
+          </div>
+        </Show>
+
         {/* Lyrics */}
         <div class="grid grid-cols-1 gap-4 lg:h-80 lg:grid-cols-3 lg:overflow-y-scroll lg:pb-2">
-          <Show
-            when={peek()}
-            fallback={<div class="h-42 rounded-md bg-gray-200/40"></div>}
-          >
+          <Show when={peek()}>
             <For each={peek()?.lyrics}>
               {(verse, index) => (
                 <LyricsCard
