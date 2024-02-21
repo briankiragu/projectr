d81eef6bd5c9ec63b186ca50bd837eb0a6c787b1
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-02-21 15:26:44 +0300

    feat: Change projection button to button

--- src/ui/buttons/ProjectionButton.tsx
+++ src/ui/buttons/ProjectionButton.tsx
@@ -9,13 +9,14 @@ const ProjectionButton: Component<{ handler?: () => void }> = (props) => {
 
   return (
     <Show when={isSupported}>
-      <div
+      <button
+        type="button"
         class="flex h-10 w-10 cursor-pointer gap-3 rounded-full bg-teal-300 p-2 text-teal-800 transition-colors hover:bg-teal-700 hover:text-teal-50 lg:h-auto lg:w-auto lg:px-6"
         onClick={props.handler}
       >
         <span class="material-symbols-outlined">screen_share</span>
         <span class="hidden font-semibold lg:inline">Project</span>
-      </div>
+      </button>
     </Show>
   );
 };
