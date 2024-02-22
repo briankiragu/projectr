e883291f01dbc04ade3f6a7b0020a5706334d495
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-02-22 12:12:54 +0300

    feat: Add ref to QueuelistItem props

--- src/ui/queue/QueueListItem.tsx
+++ src/ui/queue/QueueListItem.tsx
@@ -7,20 +7,25 @@ import type { ITrack } from '../../interfaces/track';
 import useFormatting from '../../lib/composables/useFormatting';
 
 const QueueListItem: Component<{
+  ref?: HTMLDivElement;
   track: ITrack;
   handler: () => void;
-}> = (props) => {
+}> = ({ ref, track, handler }) => {
   const { toTitleCase } = useFormatting();
 
   return (
-    <div class="flex justify-between gap-4 rounded-lg bg-gray-100 px-4 py-2 align-middle text-sm text-gray-600 shadow transition-shadow hover:shadow-md">
+    <div
+      ref={ref}
+      draggable="true"
+      class="flex justify-between gap-4 rounded-lg bg-gray-100 px-4 py-2 align-middle text-sm text-gray-600 shadow transition-shadow hover:shadow-md"
+    >
       <span class="material-symbols-outlined cursor-move p-1">drag_pan</span>
       <h4 class="col-span-2 w-full py-1 font-semibold">
-        {toTitleCase(props.track.title)}
+        {toTitleCase(track.title)}
       </h4>
       <button
         class="material-symbols-outlined rounded-full p-1 hover:bg-gray-300"
-        onClick={props.handler}
+        onClick={handler}
       >
         playlist_remove
       </button>
