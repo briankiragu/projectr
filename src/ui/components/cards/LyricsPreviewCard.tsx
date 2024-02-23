ec6dfc23acfd399ef853e22f9cace54dadba4920
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-02-23 17:24:52 +0300

    ui: Change LyricsPreviewCard background to green

--- src/ui/components/cards/LyricsPreviewCard.tsx
+++ src/ui/components/cards/LyricsPreviewCard.tsx
@@ -2,7 +2,7 @@ import { type Component, For } from 'solid-js';
 
 const LyricsPreviewCard: Component<{ verse?: string[] }> = (props) => {
   return (
-    <div class="flex h-full flex-col justify-center gap-4 rounded-lg bg-green-600 px-6 py-4 text-center text-teal-50 shadow-lg shadow-teal-600/20 transition-colors lg:min-h-72">
+    <div class="flex h-full flex-col justify-center gap-4 rounded-lg bg-green-900 px-6 py-4 text-center text-teal-50 shadow-lg shadow-teal-600/20 transition-colors lg:min-h-72">
       <For each={props.verse}>
         {(line) => (
           <p class="text-wrap text-2xl font-extrabold uppercase lg:text-4xl 2xl:text-7xl">
