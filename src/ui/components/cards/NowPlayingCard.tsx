082debb9f57e9b5e928299036e4530428b344d33
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-02-23 17:15:14 +0300

    ui: Update accent color to green on some components

--- src/ui/components/cards/NowPlayingCard.tsx
+++ src/ui/components/cards/NowPlayingCard.tsx
@@ -9,14 +9,14 @@ const NowPlayingCard: Component<{
   const { toTitleCase } = useFormatting();
 
   return (
-    <div class="relative flex h-16 items-center truncate rounded-lg bg-cyan-600 px-5 align-middle text-sm shadow-lg shadow-cyan-600/20 lg:px-6 lg:py-4">
-      <h4 class="w-[90%] text-wrap text-lg font-extrabold text-teal-50">
+    <div class="relative flex h-16 items-center truncate rounded-lg bg-green-600 px-5 align-middle text-sm shadow-lg shadow-green-600/20 lg:px-6 lg:py-4">
+      <h4 class="w-[90%] text-wrap text-lg font-extrabold text-green-50">
         {toTitleCase(props.track?.title)}
       </h4>
 
       {/* Enable live edit */}
       <span
-        class="material-symbols-outlined cursor-pointer rounded-full p-1.5 transition-colors hover:bg-cyan-700 hover:text-cyan-100"
+        class="material-symbols-outlined cursor-pointer rounded-full p-1.5 transition-colors bg-green-500 hover:bg-green-700 hover:text-green-100"
         onClick={props.handler}
       >
         edit
