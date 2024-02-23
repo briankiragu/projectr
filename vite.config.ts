6e0248a4c4d2c1ad369e740f8ae42d1500e6a769
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-02-23 09:59:46 +0300

    feat: Add alias resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,25 @@
-import { defineConfig } from "vite";
-import solid from "vite-plugin-solid";
-import { join } from "path";
-import { partytownVite } from "@builder.io/partytown/utils";
+import { defineConfig } from 'vite';
+import solid from 'vite-plugin-solid';
+import { join } from 'path';
+import { partytownVite } from '@builder.io/partytown/utils';
+import path from 'path';
 
 export default defineConfig({
   plugins: [
     solid(),
     partytownVite({
-      dest: join(__dirname, "dist", "~partytown"),
+      dest: join(__dirname, 'dist', '~partytown'),
     }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/ui/components'),
+      '@composables': path.resolve(__dirname, './src/lib/composables'),
+      '@interfaces': path.resolve(__dirname, './src/lib/interfaces'),
+      '@lib': path.resolve(__dirname, './src/lib'),
+      '@pages': path.resolve(__dirname, './src/ui/pages'),
+      '@ui': path.resolve(__dirname, './src/ui'),
+    },
+  },
 });
