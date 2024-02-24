47f125399b973f80abe36b534ed0afaad8637f33
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-02-24 22:29:53 +0300

    feat: Change service worker network cache strategy

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,19 @@
-const cacheName = 'projectr-v1.0.0';
-const appShellFiles = ['/logo.webp'];
+const cacheName = 'projectr-v1.0.1';
+const appShellFiles = [
+  '/favicons/android-chrome-36x36.png',
+  '/favicons/android-chrome-48x48.png',
+  '/favicons/android-chrome-72x72.png',
+  '/favicons/android-chrome-96x96.png',
+  '/favicons/apple-touch-icon-72x72.png',
+  '/favicons/android-chrome-144x144.png',
+  '/favicons/android-chrome-192x192.png',
+  '/favicons/android-chrome-256x256.png',
+  '/favicons/android-chrome-384x384.png',
+  '/favicons/android-chrome-512x512.png',
+  'images/long.png',
+  'images/wide.png',
+  '/logo.webp',
+];
 
 self.addEventListener('install', (e) => {
   console.info('[Service Worker] Install');
@@ -27,36 +41,35 @@ self.addEventListener('activate', (e) => {
   );
 });
 
+/* Network-first cache */
+self.addEventListener('fetch', (e) => {
+  // Open the cache
+  e.respondWith(caches.open(cacheName).then(async (cache) => {
+    // Go to the network first
+    try {
+      const fetchedResponse = await fetch(e.request.url);
+      cache.put(e.request, fetchedResponse.clone());
+      return fetchedResponse;
+    } catch {
+      return await cache.match(e.request.url);
+    }
+  }));
+});
+
+/* State-while-revalidate cache */
 // self.addEventListener('fetch', (e) => {
 //   e.respondWith(
-//     (async () => {
-//       const r = await caches.match(e.request);
+//     caches.open(cacheName).then(async (cache) => {
 //       console.info(`[Service Worker] Fetching resource: ${e.request.url}`);
-//       if (r) {
-//         return r;
-//       }
-//       const response = await fetch(e.request);
-//       const cache = await caches.open(cacheName);
-//       console.info(`[Service Worker] Caching new resource: ${e.request.url}`);
-//       cache.put(e.request, response.clone());
-//       return response;
-//     })()
+
+//       const cachedResponse = await cache.match(e.request);
+//       const fetchedResponse = fetch(e.request).then((networkResponse) => {
+//         cache.put(e.request, networkResponse.clone());
+
+//         return networkResponse;
+//       });
+
+//       return cachedResponse || fetchedResponse;
+//     })
 //   );
 // });
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.open(cacheName).then(async (cache) => {
-      console.info(`[Service Worker] Fetching resource: ${e.request.url}`);
-
-      const cachedResponse = await cache.match(e.request);
-      const fetchedResponse = fetch(e.request).then((networkResponse) => {
-        cache.put(e.request, networkResponse.clone());
-
-        return networkResponse;
-      });
-
-      return cachedResponse || fetchedResponse;
-    })
-  );
-});
