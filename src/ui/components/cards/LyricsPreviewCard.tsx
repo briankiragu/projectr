71b5792128f0468002b2f35b69be093f2a1e6269
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-02-25 08:38:27 +0300

    ui: Increase projection screen size

--- src/ui/components/cards/LyricsPreviewCard.tsx
+++ src/ui/components/cards/LyricsPreviewCard.tsx
@@ -5,7 +5,7 @@ const LyricsPreviewCard: Component<{ verse?: string[] }> = (props) => {
     <div class="flex h-full flex-col justify-center gap-4 rounded-lg bg-green-900 px-6 py-4 text-center text-teal-50 shadow-lg shadow-teal-600/20 transition-colors lg:min-h-72">
       <For each={props.verse}>
         {(line) => (
-          <p class="text-wrap text-2xl font-extrabold uppercase lg:text-6xl 2xl:text-9xl">
+          <p class="text-wrap text-2xl font-extrabold uppercase md:text-4xl lg:text-6xl">
             {line}
           </p>
         )}
