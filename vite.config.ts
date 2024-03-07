b6b58bd2ec277c872e0dbb6b300e8a86bbab33ed
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-03-07 09:55:44 +0300

    Revert "feat: Attempt to add Vercel edge functions to hit MusixMatch API"
    This reverts commit 5130ee58bcec99611e83bb12e34ffd7acebc5248.

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,23 @@
-import { defineConfig } from "vite";
-import solid from "vite-plugin-solid";
-import { join } from "path";
-import { partytownVite } from "@builder.io/partytown/utils";
-import path from "path";
+import { defineConfig } from 'vite';
+import solid from 'vite-plugin-solid';
+import { join } from 'path';
+import { partytownVite } from '@builder.io/partytown/utils';
+import path from 'path';
 
 export default defineConfig({
   plugins: [
     solid(),
     partytownVite({
-      dest: join(__dirname, "dist", "~partytown"),
+      dest: join(__dirname, 'dist', '~partytown'),
     }),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@api": path.resolve(__dirname, "./api"),
-      "@components": path.resolve(__dirname, "./src/ui/components"),
-      "@composables": path.resolve(__dirname, "./src/lib/composables"),
-      "@interfaces": path.resolve(__dirname, "./src/lib/interfaces"),
-      "@pages": path.resolve(__dirname, "./src/ui/pages"),
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/ui/components'),
+      '@composables': path.resolve(__dirname, './src/lib/composables'),
+      '@interfaces': path.resolve(__dirname, './src/lib/interfaces'),
+      '@pages': path.resolve(__dirname, './src/ui/pages'),
     },
   },
 });
