1cdf9f60c4e59463bce87a8fed675decbe804e1c
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-03-19 09:35:50 +0300

    feat: Add Meili port as env variable

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_BROADCAST_NAME: string;
 
   readonly VITE_MEILI_HOST: string;
+  readonly VITE_MEILI_PORT: string;
   readonly VITE_MEILI_MASTER_KEY: string;
 
   readonly VITE_MUSIXMATCH_API_URL: string;
