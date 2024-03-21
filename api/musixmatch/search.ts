590ed8381f6f900780546213d1847c4b0f0c835e
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-03-21 21:21:52 +0300

    feat: Initial request bootstrapping

---
+++ api/musixmatch/search.ts
@@ -0,0 +1,21 @@
+const API_URL = process.env.VITE_MUSIXMATCH_API_URL;
+
+export const GET = async (request: Request) => {
+  const queryString = request.url.split("?").at(-1);
+  const url = `${API_URL}/track.search?${queryString}`;
+
+  try {
+    // Make the request.
+    const response = await fetch(url);
+
+    // Check if the response is OK.
+    if (!response.ok) {
+      throw `Failed to make request: ${response.body}`;
+    }
+
+    // Return the response.
+    return response;
+  } catch (e) {
+    return new Response(`Making request to URL ${url}`);
+  }
+};
