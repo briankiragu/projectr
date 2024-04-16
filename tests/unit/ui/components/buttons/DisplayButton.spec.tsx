36c703fa0d4df3a0cd13dcf6cdcdefb366466fb8
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-04-16 18:27:56 +0300

    chore: Bootstrap testing utils
    Closes #27

---
+++ tests/unit/ui/components/buttons/DisplayButton.spec.tsx
@@ -0,0 +1,7 @@
+import { describe, test } from "vitest";
+// import { render, fireEvent } from "@solidjs/testing-library";
+// import DisplayButton from "@components/buttons/DisplayButton";
+
+describe("<DisplayButton />", () => {
+  test.todo("it will render text and an icon", () => {});
+});
