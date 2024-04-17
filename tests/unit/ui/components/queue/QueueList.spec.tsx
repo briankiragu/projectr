c49505a527dbb12dd55b4768acd103cff98eb912
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-04-17 21:42:58 +0300

    test: Implement tests on QueueList component

--- tests/unit/ui/components/queue/QueueList.spec.tsx
+++ tests/unit/ui/components/queue/QueueList.spec.tsx
@@ -1,11 +1,16 @@
 import QueueList from "@components/queue/QueueList";
 import type { IQueueItem } from "@interfaces/queue";
-import { cleanup, render } from "@solidjs/testing-library";
-import { afterEach, describe, test, vi } from "vitest";
+import { cleanup, render, screen } from "@solidjs/testing-library";
+import { afterEach, describe, expect, test, vi } from "vitest";
 
 describe("<QueueList />", () => {
   // Declare the component props.
-  const queue: IQueueItem[] = [];
+  const title = "thIs IS-tHE-tiTLe";
+  const line = "This is a line from a verse";
+  const queue: IQueueItem[] = [
+    { qid: Date.now(), title, content: [[line]] },
+    { qid: Date.now(), title, content: [[line]] },
+  ];
   const playFn = vi.fn();
   const queueFn = vi.fn();
 
@@ -13,8 +18,15 @@ describe("<QueueList />", () => {
   afterEach(() => cleanup());
 
   test("it renders correctly", () => {
+    // Render the component on the vDOM.
     render(() => (
       <QueueList queue={queue} playHandler={playFn} queueHandler={queueFn} />
     ));
+
+    // Get the element from the vDOM.
+    const el = screen.getByTestId("queue-list");
+
+    // Make the assertions.
+    expect(el).toBeInTheDocument();
   });
 });
