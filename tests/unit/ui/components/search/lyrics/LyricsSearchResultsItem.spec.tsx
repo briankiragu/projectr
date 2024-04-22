405ecd20686fabf9c3166656a1fef05d00fff045
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-04-22 18:28:13 +0300

    test: Test that the handler is called when the button is clicked

--- tests/unit/ui/components/search/lyrics/LyricsSearchResultsItem.spec.tsx
+++ tests/unit/ui/components/search/lyrics/LyricsSearchResultsItem.spec.tsx
@@ -1,6 +1,7 @@
 import LyricsSearchResultsItem from "@components/search/lyrics/LyricsSearchResultsItem";
 import { ISource, IStatus, type ISearchItem } from "@interfaces/track";
 import { render, screen, within } from "@solidjs/testing-library";
+import userEvent from "@testing-library/user-event";
 import { describe, expect, test, vi } from "vitest";
 
 describe("<LyricsSearchResultsItem />", () => {
@@ -16,6 +17,9 @@ describe("<LyricsSearchResultsItem />", () => {
   };
   const handlerFn = vi.fn();
 
+  // Define the user for events.
+  const user = userEvent.setup();
+
   test("it renders correctly", () => {
     // Render the component in the vDOM.
     render(() => <LyricsSearchResultsItem track={track} handler={handlerFn} />);
@@ -23,9 +27,25 @@ describe("<LyricsSearchResultsItem />", () => {
     // Get the component from the vDOM.
     const listItemEl = screen.getByRole("listitem");
     const imgEl = within(listItemEl).getByRole("img");
+    const buttonEl = within(listItemEl).getByRole("button");
 
     // Make the assertions.
     expect(listItemEl).toHaveTextContent("This Is The Title");
     expect(imgEl).toHaveAttribute("src", "/images/MusixMatch-logo.webp");
+    expect(buttonEl).toBeInTheDocument();
+  });
+
+  test("it calls the handler when clicked", async () => {
+    // Render the component in the vDOM.
+    render(() => <LyricsSearchResultsItem track={track} handler={handlerFn} />);
+
+    // Get the component from the vDOM.
+    const el = screen.getByRole("button");
+
+    // Interact with the vDOM.
+    await user.click(el);
+
+    // Make the assertions
+    expect(handlerFn).toHaveBeenCalledOnce();
   });
 });
