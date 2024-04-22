448b7bdfbf4b2b7644e025d9f3effb4c50887b79
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-04-22 18:20:35 +0300

    test: Test if the track details are rendered correctly

---
+++ tests/unit/ui/components/search/lyrics/LyricsSearchResultsItem.spec.tsx
@@ -0,0 +1,31 @@
+import LyricsSearchResultsItem from "@components/search/lyrics/LyricsSearchResultsItem";
+import { ISource, IStatus, type ISearchItem } from "@interfaces/track";
+import { render, screen, within } from "@solidjs/testing-library";
+import { describe, expect, test, vi } from "vitest";
+
+describe("<LyricsSearchResultsItem />", () => {
+  // Define the component props.
+  const title = "ThIs IS-tHe-tItLe";
+  const line = "This is a line";
+  const track: ISearchItem = {
+    title,
+    lyrics: [[line]],
+    source: ISource.musix,
+    status: IStatus.PUBLISHED,
+    sort: null,
+  };
+  const handlerFn = vi.fn();
+
+  test("it renders correctly", () => {
+    // Render the component in the vDOM.
+    render(() => <LyricsSearchResultsItem track={track} handler={handlerFn} />);
+
+    // Get the component from the vDOM.
+    const listItemEl = screen.getByRole("listitem");
+    const imgEl = within(listItemEl).getByRole("img");
+
+    // Make the assertions.
+    expect(listItemEl).toHaveTextContent("This Is The Title");
+    expect(imgEl).toHaveAttribute("src", "/images/MusixMatch-logo.webp");
+  });
+});
