cc1ff228c46ceeeca38f0f1e16bb808f48c44570
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-04-22 18:20:35 +0300

    test: Test if the track details are rendered correctly

--- src/ui/components/search/lyrics/LyricsSearchResultsItem.tsx
+++ src/ui/components/search/lyrics/LyricsSearchResultsItem.tsx
@@ -13,7 +13,7 @@ const LyricsSearchResultsItem: Component<{
   const { toTitleCase } = useFormatting();
 
   return (
-    <div class="flex items-center justify-between gap-5 rounded-lg bg-gray-100 px-4 py-2 text-sm text-gray-600 shadow transition-shadow hover:shadow-md">
+    <li class="flex items-center justify-between gap-5 rounded-lg bg-gray-100 px-4 py-2 text-sm text-gray-600 shadow transition-shadow hover:shadow-md">
       <div class="col-span-2 flex flex-col">
         <h4 class="font-semibold">{toTitleCase(props.track.title)}</h4>
         <span class="text-sm font-light italic">
@@ -35,7 +35,7 @@ const LyricsSearchResultsItem: Component<{
           add_to_queue
         </button>
       </div>
-    </div>
+    </li>
   );
 };
 
