280e6155bbae9f4827552503a1f07f0e8e4bf660
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-04-27 14:08:51 +0300

    test: Test the useQueue composable
    Test cases:
    * it returns the first value in the queue
    * it adds an item to the queue
    * it removes an item from the queue
    * it empties the queue
    * it verifies if the current verse is the first
    * it verifies if the current verse is the last
    * it moves to the previous verse
    * it moves to the next verse
    * it moves to a specific verse
    
    Closes #101

---
+++ tests/unit/composables/useQueue.spec.ts
@@ -0,0 +1,21 @@
+import { describe, test } from "vitest";
+
+describe("useQueue", () => {
+  test.todo("it returns the first value in the queue", () => {});
+
+  test.todo("it adds an item to the queue", () => {});
+
+  test.todo("it removes an item from the queue", () => {});
+
+  test.todo("it empties the queue", () => {});
+
+  test.todo("it verifies if the current verse is the first", () => {});
+
+  test.todo("it verifies if the current verse is the last", () => {});
+
+  test.todo("it moves to the previous verse", () => {});
+
+  test.todo("it moves to the next verse", () => {});
+
+  test.todo("it moves to a specific verse", () => {});
+});
