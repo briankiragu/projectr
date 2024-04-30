7edb708f4340db9647536abf410ca60d307bb7fe
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-04-30 15:05:22 +0300

    ui: Change disabled styles on ProjectionButton

--- src/ui/components/buttons/ProjectionButton.tsx
+++ src/ui/components/buttons/ProjectionButton.tsx
@@ -10,9 +10,11 @@ const ProjectionButton: Component<{
   <button
     type="button"
     title={props.title}
-    class="relative flex cursor-pointer gap-3 rounded-full bg-teal-300 p-2 transition-colors hover:bg-teal-700 hover:text-teal-50 focus:outline-none md:h-auto md:w-auto md:px-6"
+    class="relative flex cursor-pointer gap-3 rounded-full  p-2 transition-colors hover:bg-teal-700 hover:text-teal-50 focus:outline-none md:h-auto md:w-auto md:px-6"
     classList={{
-      disabled: !props.isAvailable,
+      "bg-teal-300": props.isAvailable,
+      "hover:bg-transparent disabled:text-gray-300 hover:cursor-not-allowed":
+        !props.isAvailable,
       "bg-teal-700 text-teal-50 hover:bg-indigo-500 hover:text-indigo-50":
         props.isProjecting,
     }}
