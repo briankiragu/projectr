26636263e0e4780dfd67d520f945006bf5515f67
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-04-30 15:58:03 +0300

    enhancement: Add a second button to the ProjectionButton component
    To faciliate multiple presentation sessions, the one button is not sufficient; we need two buttons for start and stop

--- src/ui/components/buttons/ProjectionButton.tsx
+++ src/ui/components/buttons/ProjectionButton.tsx
@@ -7,29 +7,44 @@ const ProjectionButton: Component<{
   startHandler: () => void;
   stopHandler: () => void;
 }> = (props) => (
-  <button
-    type="button"
-    title={props.title}
-    class="relative flex cursor-pointer gap-3 rounded-full  p-2 transition-colors hover:bg-teal-700 hover:text-teal-50 focus:outline-none md:h-auto md:w-auto md:px-6"
-    classList={{
-      "bg-teal-300": props.isAvailable,
-      "hover:bg-transparent disabled:text-gray-300 hover:cursor-not-allowed":
-        !props.isAvailable,
-      "bg-teal-700 text-teal-50 hover:bg-indigo-500 hover:text-indigo-50":
-        props.isProjecting,
-    }}
-    disabled={!props.isAvailable}
-    onClick={() =>
-      props.isProjecting ? props.stopHandler() : props.startHandler()
-    }
-  >
-    <span class="material-symbols-outlined transition">
-      {props.isProjecting ? "stop_screen_share" : "screen_share"}
-    </span>
-    <span class="hidden font-semibold md:inline">
-      {props.isProjecting ? "Stop projecting" : "Project"}
-    </span>
-  </button>
+  <div class="relative flex gap-0.5">
+    {/* Start projection */}
+    <button
+      type="button"
+      title={props.title}
+      class="flex cursor-pointer gap-3 rounded-l-full p-2 pl-3 transition-colors hover:bg-teal-700 hover:text-teal-50 focus:outline-none md:h-auto md:w-auto md:px-6"
+      classList={{
+        "bg-teal-300": props.isAvailable,
+        "hover:bg-transparent disabled:text-gray-300 hover:cursor-not-allowed":
+          !props.isAvailable,
+      }}
+      disabled={!props.isAvailable}
+      onClick={() => props.startHandler()}
+    >
+      <span class="material-symbols-outlined transition">screen_share</span>
+      <span class="hidden font-semibold md:inline">Project</span>
+    </button>
+
+    {/* Stop projection */}
+    <button
+      type="button"
+      title={props.title}
+      class="flex cursor-pointer gap-3 rounded-r-full p-2 pr-3 transition-colors focus:outline-none md:h-auto md:w-auto md:px-6"
+      classList={{
+        "bg-teal-700 text-teal-50 hover:bg-pink-700 hover:text-pink-50":
+          props.isProjecting,
+        "hover:bg-transparent disabled:text-gray-300 hover:cursor-not-allowed":
+          !props.isProjecting,
+      }}
+      disabled={!props.isProjecting}
+      onClick={() => props.stopHandler()}
+    >
+      <span class="material-symbols-outlined transition">
+        stop_screen_share
+      </span>
+      <span class="hidden font-semibold md:inline">Stop projecting</span>
+    </button>
+  </div>
 );
 
 export default ProjectionButton;
