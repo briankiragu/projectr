acb2a51c3711829455a7fe1c246c9b5931dade2d
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-05-13 15:52:06 +0300

    ui: Fix scrolling issue for search section

--- src/ui/pages/Controller.tsx
+++ src/ui/pages/Controller.tsx
@@ -1,22 +1,46 @@
-import OfflineBanner from "@components/banners/OfflineBanner";
+import { type Component } from "solid-js";
+
+// Import the components...
 import DisplayButton from "@components/buttons/DisplayButton";
+import OfflineBanner from "@components/banners/OfflineBanner";
 import PlaybackButton from "@components/buttons/PlaybackButton";
 import ProjectionButton from "@components/buttons/ProjectionButton";
-import type { Component } from "solid-js";
+import { For } from "solid-js";
 
 const Controller: Component = () => {
   return (
-    <div class="flex h-dvh flex-col justify-between">
+    <div class="flex h-dvh flex-col">
       {/* Offline Banner */}
       <OfflineBanner isOffline={true} />
 
-      <div class="grid h-full grid-cols-1 bg-gray-100">
+      <div class="grid grow grid-cols-1 bg-gray-400 lg:grid-cols-4">
         {/* Sidebar */}
-        <aside class="col-span-1 h-full bg-gray-200"></aside>
+        <aside class="col-span-1 flex flex-col bg-rose-400">
+          {/* Searchbar and search results */}
+          <search class="flex h-[50%] flex-col bg-yellow-400">
+            {/* Searchbar */}
+            <form class="bg-pink-400">
+              <input
+                type="search"
+                class="w-full"
+                placeholder="Search for a track by title, lyrics or author"
+              />
+            </form>
 
-        <main class="col-span-1 flex flex-col justify-between bg-gray-300">
+            {/* Search results */}
+            <ul class="overflow-y-scroll bg-indigo-400">
+              <For each={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}>
+                {(item) => <li class="h-16 bg-indigo-600">{item}</li>}
+              </For>
+            </ul>
+          </search>
+
+          <div class="h-[50%] bg-green-400"></div>
+        </aside>
+
+        <main class="col-span-1 flex flex-col justify-between bg-gray-400 lg:col-span-3">
           {/* Display */}
-          <section class="bg-gray-400"></section>
+          <section class="grow overflow-y-scroll bg-fuchsia-400"></section>
 
           {/* Controls */}
           <footer class="sticky bottom-0 bg-white p-3">
