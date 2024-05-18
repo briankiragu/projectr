277e2e3fbab76aa32dc64e5fcbbb297d973384f8
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-05-18 16:37:03 +0300

    ui: Test WebShareAPI

--- src/ui/components/buttons/ShareButton.tsx
+++ src/ui/components/buttons/ShareButton.tsx
@@ -10,10 +10,12 @@ const ShareButton: Component<{
   <button
     type="button"
     title={`Share ${props.item.title}`}
-    class="flex size-12 items-center justify-center gap-2 rounded-full bg-tvc-orange font-bold text-white transition-colors hover:bg-tvc-green hover:text-gray-50 focus:outline-none"
+    class="relative bottom-1.5 flex items-center justify-center gap-2 rounded-full px-2 py-1.5 text-black transition-colors hover:text-tvc-orange focus:outline-none"
     onClick={() => props.shareHandler(props.item)}
   >
-    <span class="material-symbols-outlined text-3xl transition">share</span>
+    <span class="material-symbols-outlined text-3xl font-bold transition">
+      share
+    </span>
   </button>
 );
 
