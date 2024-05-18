fe17d369201802ed0f57a58b0056e5bf3d095ea2
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-05-18 16:31:29 +0300

    enhancement: Test API

--- src/lib/composables/apis/useWebShareAPI.ts
+++ src/lib/composables/apis/useWebShareAPI.ts
@@ -4,9 +4,8 @@ export default () => {
   const { requestWebSharePermissions } = usePermissionsAPI();
 
   const share = async (data: { title: string; text: string }) => {
-    // navigator.canShare(data) &&
     requestWebSharePermissions();
-    return await navigator.share(data);
+    return navigator.canShare(data) && (await navigator.share(data));
   };
 
   return { share };
