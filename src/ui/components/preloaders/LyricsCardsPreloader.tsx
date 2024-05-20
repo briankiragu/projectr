4996d1af906379ea463e0af20e2c97df8c652738
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2024-05-20 18:00:03 +0300

    ui: Center align assistive text

--- src/ui/components/preloaders/LyricsCardsPreloader.tsx
+++ src/ui/components/preloaders/LyricsCardsPreloader.tsx
@@ -3,11 +3,11 @@ import { type Component } from "solid-js";
 
 const LyricsCardsPreloader: Component<{ canProject: boolean }> = (props) => {
   return (
-    <div class="flex flex-1 items-center justify-center text-lg font-semibold tracking-tight text-gray-800">
+    <div class="flex flex-1 items-center justify-center text-center text-lg font-semibold tracking-tight text-gray-800">
       <Show
         when={props.canProject}
         fallback={
-          <div class="flex flex-col items-center justify-center">
+          <div class="flex flex-col items-center justify-center gap-4">
             <img src="/images/failed.png" class="h-auto w-44 opacity-90" />
 
             <h3>Failed to identify/project onto a second screen.</h3>
