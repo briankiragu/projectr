37014efbe98e560e66ff82bb341088356f48a63c
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2025-03-14 14:43:07 +0300

    ui: Update search results ui

--- src/ui/components/search/lyrics/LyricsSearchResultsItem.tsx
+++ src/ui/components/search/lyrics/LyricsSearchResultsItem.tsx
@@ -14,16 +14,18 @@ const LyricsSearchResultsItem: Component<{
 
   return (
     <li class="flex items-center justify-between gap-5 rounded-lg bg-gray-100 px-4 py-2 text-sm text-gray-600 shadow transition-shadow hover:shadow-md dark:bg-gray-300 dark:text-gray-800">
-      <div class="col-span-2 flex flex-col gap-0.5">
-        <h4 class="font-semibold">{toTitleCase(props.track.title)}</h4>
-        <Show when={props.track.artists?.length}>
-          <span
-            class="oblique text-xs font-light"
-            innerHTML={toTitleCase(props.track.artists?.join(", ")) || ""}
-          ></span>
-        </Show>
+      <div class="col-span-2 flex flex-col gap-1">
+        <div class="div flex flex-col gap-0">
+          <h4 class="font-bold">{toTitleCase(props.track.title)}</h4>
+          <Show when={props.track.artists?.length}>
+            <span
+              class="oblique text-xs font-light"
+              innerHTML={toTitleCase(props.track.artists?.join(", ")) || ""}
+            ></span>
+          </Show>
+        </div>
         <span
-          class="text-sm font-medium italic leading-tight"
+          class="text-sm font-normal italic leading-tight"
           innerHTML={toTitleCase(props.track.content[0][0]) || ""}
         ></span>
       </div>
