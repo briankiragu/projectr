e9f2d553b038fa6658b5a1270d7f977419f6bf05
Brian K. Kiragu <briankiragu@microsoft.com>
2025-12-22 13:44:30 +0300

    refactor: Consolidate imports and improve formatting in NowPlayingCard and LyricsCardsPreloader components

--- src/ui/components/cards/NowPlayingCard.tsx
+++ src/ui/components/cards/NowPlayingCard.tsx
@@ -1,31 +1,32 @@
-import { type Component } from "solid-js";
+import type { Component } from "solid-js";
 import type { IQueueItem } from "@interfaces/queue";
 import useFormatting from "@composables/useFormatting";
 
 const NowPlayingCard: Component<{
-  item: IQueueItem;
-  handler: () => void;
+	item: IQueueItem;
+	handler: () => void;
 }> = (props) => {
-  const { toTitleCase } = useFormatting();
+	const { toTitleCase } = useFormatting();
 
-  return (
-    <div
-      data-testid="now-playing-card"
-      class="relative flex min-h-16 items-center gap-0.5 truncate rounded-lg bg-tvc-green px-5 py-2 align-middle text-sm tracking-tight shadow-lg shadow-tvc-green/20 md:px-4 xl:px-4 dark:bg-teal-700"
-    >
-      <h4 class="w-[90%] text-wrap text-lg font-extrabold text-white md:text-sm xl:text-lg">
-        {toTitleCase(props.item.title)}
-      </h4>
+	return (
+		<div
+			data-testid="now-playing-card"
+			class="relative flex min-h-16 items-center gap-0.5 truncate rounded-lg bg-tvc-green px-5 py-2 align-middle text-sm tracking-tight shadow-lg shadow-tvc-green/20 md:px-4 xl:px-4 dark:bg-teal-700"
+		>
+			<h4 class="w-[90%] text-wrap text-lg font-extrabold text-white md:text-sm xl:text-lg">
+				{toTitleCase(props.item.title)}
+			</h4>
 
-      {/* Enable live edit */}
-      <button
-        class="material-symbols-outlined cursor-pointer rounded-full bg-tvc-green p-1.5 transition-colors hover:bg-tvc-orange hover:text-orange-100 dark:hover:bg-orange-600"
-        onClick={() => props.handler()}
-      >
-        edit
-      </button>
-    </div>
-  );
+			{/* Enable live edit */}
+			<button
+				type="button"
+				class="material-symbols-outlined cursor-pointer rounded-full bg-tvc-green p-1.5 transition-colors hover:bg-tvc-orange hover:text-orange-100 dark:hover:bg-orange-600"
+				onClick={() => props.handler()}
+			>
+				edit
+			</button>
+		</div>
+	);
 };
 
 export default NowPlayingCard;
--- src/ui/components/preloaders/LyricsCardsPreloader.tsx
+++ src/ui/components/preloaders/LyricsCardsPreloader.tsx
@@ -1,38 +1,37 @@
-import { Show } from "solid-js";
-import { type Component } from "solid-js";
+import { type Component, Show } from "solid-js";
 
 const LyricsCardsPreloader: Component<{ canProject: boolean }> = (props) => {
-  return (
-    <div class="flex flex-1 items-center justify-center text-center text-lg font-semibold tracking-tight text-gray-800">
-      <Show
-        when={props.canProject}
-        fallback={
-          <div class="flex flex-col items-center justify-center gap-4">
-            <img
-              src="/images/failed.webp"
-              alt="Failed to identify second screen"
-              class="w-44 p-4 opacity-90 dark:rounded-full dark:bg-gray-400/40"
-            />
+	return (
+		<div class="flex flex-1 items-center justify-center text-center text-lg font-semibold tracking-tight text-gray-800">
+			<Show
+				when={props.canProject}
+				fallback={
+					<div class="flex flex-col items-center justify-center gap-4">
+						<img
+							src="/images/failed.webp"
+							alt="Failed to identify second screen"
+							class="w-44 p-4 opacity-90 dark:rounded-full dark:bg-gray-400/40"
+						/>
 
-            <h3 class="dark:text-gray-200">
-              Failed to identify a second screen.
-            </h3>
-          </div>
-        }
-      >
-        <div class="flex flex-col items-center justify-center gap-4">
-          <img
-            src="/images/waiting.webp"
-            alt="No items in queue"
-            class="w-32 p-4 opacity-90 dark:rounded-full dark:bg-gray-400/40"
-          />
-          <h3 class="dark:text-gray-200">
-            Waiting for content to be queued...
-          </h3>
-        </div>
-      </Show>
-    </div>
-  );
+						<h3 class="dark:text-gray-200">
+							Failed to identify a second screen.
+						</h3>
+					</div>
+				}
+			>
+				<div class="flex flex-col items-center justify-center gap-4">
+					<img
+						src="/images/waiting.webp"
+						alt="No items in queue"
+						class="w-32 p-4 opacity-90 dark:rounded-full dark:bg-gray-400/40"
+					/>
+					<h3 class="dark:text-gray-200">
+						Waiting for content to be queued...
+					</h3>
+				</div>
+			</Show>
+		</div>
+	);
 };
 
 export default LyricsCardsPreloader;
